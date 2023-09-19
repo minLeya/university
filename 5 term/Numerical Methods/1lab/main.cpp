96ed0db2b3d9e00d8b48f59774edfd00cd4ba4f0
Leya <108834527+minLeya@users.noreply.github.com>
2023-09-19 22:10:31 +0300

    Add files via upload

--- 5 term/Numerical Methods/1lab/main.cpp
+++ 5 term/Numerical Methods/1lab/main.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
-//#include "packages/Aspose.Cells.Cpp.23.9.0/build/native/include/Aspose.Cells.h"
 
 namespace constants
 {
@@ -30,6 +29,21 @@ double getNextY(double lastX, double lastZ)
 double getNextZ(double lastX, double lastY)
 {
     return 17.0 / 4.0 - lastX / 2.0 + lastY / 4.0;
+}
+
+double getResidualX(double x, double y, double z)
+{
+    return 5.0 / 4.0 - x + y / 4.0 + z / 4.0;
+}
+
+double getResidualY(double x, double y, double z)
+{
+    return -25.0 / 31.0 - y + x * 4.0 / 31.0 - z * 21.0 / 31.0;
+}
+
+double getResidualZ(double x, double y, double z)
+{
+    return -5.0 / 3.0 - z + x * 2.0 / 3.0 - y / 6.0;
 }
 
 void runIterativeMethodLoop(const double epsilon, std::ofstream& outputFile,
@@ -131,9 +145,70 @@ void runSeidelMethod(std::ofstream& outputFile, double lastX, double lastY, doub
     outputFile << '\n';
 }
 
+void printRelaxationMethodTableHeader(std::ofstream& outputFile)
+{
+    outputFile << "k" << " ;" << "x_k" << "\t;" << "y_k" << " ;" << "z_k" << " ;" <<
+        "R_x(k)" << " ;" << "R_y(k)" << " ;" << "R_z(k)" << " ;" << "max|R(k)|\n";
+}
+
+double getMaxResidualValue(double residualX, double residualY, double residualZ)
+{
+    double absoluteResidualX{ std::abs(residualX) };
+    double absoluteResidualY{ std::abs(residualY) };
+    double absoluteResidualZ{ std::abs(residualZ) };
+
+    double maxResidualAbsoluteValue{ std::max(absoluteResidualX, absoluteResidualY) };
+    maxResidualAbsoluteValue = std::max(maxResidualAbsoluteValue, absoluteResidualZ);
+
+    return maxResidualAbsoluteValue;
+}
+
+void runRelaxationMethodLoop(const double epsilon, std::ofstream& outputFile,
+    double x, double y, double z)
+{
+    int currentIteration{ 1 };
+    bool keepGoing{ true };
+    while (keepGoing)
+    {
+        double residualX{ getResidualX(x, y, z) };
+        double residualY{ getResidualY(x, y, z) };
+        double residualZ{ getResidualZ(x, y, z) };
+
+        double maxResidualAbsoluteValue{ getMaxResidualValue(residualX, residualY, residualZ) };
+
+        outputFile << currentIteration << ";" << x << ";" << y << ";" << z <<
+            ";" << residualX << ";" << residualY << ";" << residualZ << ";" <<
+            maxResidualAbsoluteValue << '\n';
+
+        ++currentIteration;
+        if (std::abs(residualX) == maxResidualAbsoluteValue)
+            x += residualX;
+        else if (std::abs(residualY) == maxResidualAbsoluteValue)
+            y += residualY;
+        else if (std::abs(residualZ) == maxResidualAbsoluteValue)
+            z += residualZ;
+
+        if (std::abs(residualX) <= epsilon && std::abs(residualY) <= epsilon &&
+            std::abs(residualZ) <= epsilon)
+        {
+            keepGoing = false;
+        }
+    }
+
+}
+
+void runRelaxationMethod(std::ofstream& outputFile, double x, double y,
+    double z)
+{
+    printRelaxationMethodTableHeader(outputFile);
+    runRelaxationMethodLoop(constants::firstEpsilon, outputFile, x, y, z);
+    outputFile << '\n';
+    printRelaxationMethodTableHeader(outputFile);
+    runRelaxationMethodLoop(constants::secondEpsilon, outputFile, x, y, z);
+}
+
 int main()
 {
-
     setlocale(LC_ALL, "Russian");
 
     std::ofstream outputFile{ "output.csv" };
@@ -142,13 +217,10 @@ int main()
 
     auto [firstX, firstY, firstZ] { getInitialValues() };
 
-    double lastX = firstX;
-    double lastY = firstY;
-    double lastZ = firstZ;
-
     printIterativeMethodTableHeader(outputFile);
-    runIterativeMethod(outputFile, lastX, lastY, lastZ);
-    runSeidelMethod(outputFile, lastX, lastY, lastZ);
+    runIterativeMethod(outputFile, firstX, firstY, firstZ);
+    runSeidelMethod(outputFile, firstX, firstY, firstZ);
+    runRelaxationMethod(outputFile, firstX, firstY, firstZ);
 
     return 0;
 }
