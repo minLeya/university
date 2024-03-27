647a003b111e38bbfa321ac406168c220552fc56
Leya <108834527+minLeya@users.noreply.github.com>
2024-03-28 00:39:59 +0300

    final ver of 3rd lab java

--- 6 term/java/ThirdLab.java
+++ 6 term/java/ThirdLab.java
@@ -1,15 +1,19 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
+// C:\Users\Лейсан\Desktop\input.txt
+
+// Обращение к потоку вывода на консоль
 interface IEventConsoleOutput {
     void Handler(String message, File output);
 }
 
+// Равенство указанного объекта некоторому значению
 interface IEventEqual {
     void Handler(String message, File output);
 }
 
+// Обращение к потоку вывода в файл
 interface IEventFileOutput {
     void Handler(String message, File output);
 }
@@ -20,7 +24,7 @@ class SourceConsoleOutput {
         this.iEvent = iEvent;
     }
     void genEv(String message, File output){
-        iEvent.Handler("Event " + message, output);
+        iEvent.Handler("Event Console Output: " + message, output);
     }
 }
 
@@ -30,7 +34,7 @@ class SourceEqual {
         this.iEvent = iEvent;
     }
     void genEv(String message, File output) {
-        iEvent.Handler("Event " + message, output);
+        iEvent.Handler("Event Equals 0: " + message, output);
     }
 }
 
@@ -41,7 +45,7 @@ class SourceFileOutput {
         this.iEvent = iEvent;
     }
     void genEv(String message, File output) {
-        iEvent.Handler("Event " + message, output);
+        iEvent.Handler("Event File Output: " + message, output);
     }
 }
 
@@ -58,9 +62,9 @@ class ReceiverConsoleOutput implements IEventConsoleOutput {
 }
 
 class ReceiverEqual implements IEventEqual {
-    public void Handler(String message, File log_file){
+    public void Handler(String message, File output){
         try {
-            PrintWriter pw = new PrintWriter(new FileWriter(log_file.getAbsoluteFile(), true));
+            PrintWriter pw = new PrintWriter(new FileWriter(output.getAbsoluteFile(), true));
             pw.println(message);
             pw.close();
         } catch (IOException e) {
@@ -70,9 +74,9 @@ class ReceiverEqual implements IEventEqual {
 }
 
 class ReceiverFileOutput implements IEventFileOutput {
-    public void Handler(String message, File log_file){
+    public void Handler(String message, File output){
         try {
-            PrintWriter pw = new PrintWriter(new FileWriter(log_file.getAbsoluteFile(), true));
+            PrintWriter pw = new PrintWriter(new FileWriter(output.getAbsoluteFile(), true));
             pw.println(message);
             pw.close();
         } catch (IOException e) {
@@ -82,7 +86,6 @@ class ReceiverFileOutput implements IEventFileOutput {
 }
 
 class DataProcessor {
-    ArrayList<Integer> array;
     private File input;
     private File output;
     SourceConsoleOutput eventConsoleOutput;
@@ -103,6 +106,7 @@ class DataProcessor {
         this.output = output;
     }
 
+    // Метод для создания новых/чтения уже существующих файлов.
     private void filesProcessing() {
         String filePath;
         File outputFile;
@@ -113,35 +117,25 @@ class DataProcessor {
                 if (!input.isEmpty()) {
                     filePath = input;
                     File inputFile = new File(filePath);
-                    if (!inputFile.exists()) {
-                        System.out.println("The input file does not exist. Path: " + inputFile.getAbsolutePath());
-                        System.out.print("Enter another path of input data file: ");
-                    } else {
-                        try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFile))) {
-                            String firstLine = fileReader.readLine();
-                            if (!firstLine.isEmpty()) {
-                                System.out.println("O: First line of the input data file is the path to the log file. Path: " + firstLine);
-                                this.setInputFile(inputFile);
-                                outputFile = new File(firstLine);
-                                this.setOutputFile(outputFile);
-                                if (!outputFile.exists()) {
-                                    outputFile.createNewFile();
-                                    System.out.println("O: Log file created successfully: " + outputFile.getAbsolutePath());
-                                    eventConsoleOutput.genEv("O: Log file created successfully: " + outputFile.getAbsolutePath(), outputFile);
-                                    System.out.println("O: The log file can be succesfully opened for logging.");
-                                    eventConsoleOutput.genEv("O: The log file can be succesfully opened for logging.", outputFile);
-                                }
-                                return;
-                            } else {
-                                System.out.println("O: Error: The input data file does not contain the path of log file. Search another input data file.");
-                                System.out.print("I/O: Path of input data file: ");
-                            }
-                        } catch (IOException e) {
-                            System.out.println("O: Error reading the file: " + e.getMessage());
+                    try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFile))) {
+                        String firstLine = fileReader.readLine();
+                        if (!firstLine.isEmpty()) {
+                            this.setInputFile(inputFile);
+                            outputFile = new File(firstLine);
+                            this.setOutputFile(outputFile);
+                            return;
+                        } else {
+                            System.out.println("Error: The input data file does not contain the path of output file.");
+                            eventConsoleOutput.genEv("Error: The input data file does not contain the path of output file.", this.output);
                         }
+                    } catch (IOException e) {
+                        System.out.println("Error reading the file: " + e.getMessage());
+                        eventConsoleOutput.genEv("Error reading the file: ", this.output);
                     }
+
                 } else {
-                    System.out.print("O: The file path was not retrieved correctly. Re-enter: ");
+                    System.out.print("The file is empty");
+                    eventConsoleOutput.genEv("The file is empty", this.output);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -149,42 +143,61 @@ class DataProcessor {
         }
     }
 
+    // Метод для сортировки и генерации событий
     public void numberProcessing(String[] data) {
         int[] numbers = new int[data.length];
+        boolean isContainZero = false;
         for (int i = 0; i < data.length; i++) {
             try {
                 int num = Integer.parseInt(data[i]);
                 numbers[i] = num;
+                if (num == 0) {
+                    isContainZero = true;
+                }
+
             } catch (NumberFormatException e) {
-                System.out.println("O: Unhandled exception happened.");
-                eventConsoleOutput.genEv("O: Unhandled exception happened.", this.output);
-                System.exit(1);
+                System.out.println("Unhandled exception happened.");
+                eventConsoleOutput.genEv("Unhandled exception happened.", this.output);
             }
         }
 
         // Сортировка массива чисел по возрастанию
         Arrays.sort(numbers);
 
+        // Событие - равенство указанного объекта некоторому значению
+        if (isContainZero) {
+            System.out.println("There is number that equals 0.");
+            eventConsoleOutput.genEv("There is number that equals 0.", this.output);
+            eventEqual.genEv("There is number that equals 0.", this.output);
+        }
+
         // Вывод отсортированного массива
         System.out.println("Sorted array: " + Arrays.toString(numbers));
-        eventConsoleOutput.genEv("O: Sorted array: " + Arrays.toString(numbers), this.output);
+        eventConsoleOutput.genEv("Sorted array: " + Arrays.toString(numbers), this.output);
     }
 
+    // Метод для обработки событий и инициализации файлов
     public void startProcessing() {
-        System.out.print("I/O: Enter the absolute path of the input file: ");
+        System.out.print("Enter path of the input file: ");
+        //eventConsoleOutput.genEv("Enter path of the input file: ", this.output);
         this.filesProcessing();
 
         if (this.output.length() != 0) {
             try {
                 PrintWriter pw = new PrintWriter(new FileWriter(this.output, true));
                 pw.println();
-                pw.println("/*New log to the same file:*/");
+                pw.println("-NEW LOG-");
+                pw.flush();
+                eventFileOutput.genEv("Using of PrintWriter", this.output);
+                System.out.println("Using of PrintWriter");
+                eventConsoleOutput.genEv("Using of PrintWriter", this.output);
                 pw.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+        // Для данных из input
         StringBuilder sb_data = new StringBuilder();
         if (this.input.exists()) {
             try (BufferedReader br_data = new BufferedReader(new FileReader(this.input.getAbsoluteFile()))) {
@@ -204,29 +217,22 @@ class DataProcessor {
             }
         }
 
+        // Преобразование sb_data в массив строк с пробелами
         this.numberProcessing(sb_data.toString().split("\\s+|\n"));
     }
 }
 
-
-
-
 public class ThirdLab {
 
     public static void main(String[] args) {
+        ReceiverConsoleOutput receiverConsoleOutput = new ReceiverConsoleOutput();
+        ReceiverEqual receiverEqual = new ReceiverEqual();
+        ReceiverFileOutput receiverFileOutput = new ReceiverFileOutput();
+        SourceEqual sourceEqual = new SourceEqual(receiverEqual);
+        SourceConsoleOutput sourceConsoleOutput = new SourceConsoleOutput(receiverConsoleOutput);
+        SourceFileOutput sourceFileOutput = new SourceFileOutput(receiverFileOutput);
 
-            ReceiverConsoleOutput receiverConsoleOutput = new ReceiverConsoleOutput();
-            ReceiverEqual receiverEqual = new ReceiverEqual();
-            ReceiverFileOutput receiverFileOutput = new ReceiverFileOutput();
-            SourceEqual sourceEqual = new SourceEqual(receiverEqual);
-            SourceConsoleOutput sourceConsoleOutput = new SourceConsoleOutput(receiverConsoleOutput);
-            SourceFileOutput sourceFileOutput = new SourceFileOutput(receiverFileOutput);
-
-
-            DataProcessor dataProcessor = new DataProcessor(sourceConsoleOutput, sourceEqual, sourceFileOutput);
-            dataProcessor.startProcessing();
-
-
-
+        DataProcessor dataProcessor = new DataProcessor(sourceConsoleOutput, sourceEqual, sourceFileOutput);
+        dataProcessor.startProcessing();
     }
 }
