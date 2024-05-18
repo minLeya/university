76c7ed07f7d73ca2830aea321d38557079a4bf9e
Leya <108834527+minLeya@users.noreply.github.com>
2024-05-18 21:26:44 +0300

    add 1st lab java

---
+++ 6 term/java/1st-lab/FirstLab.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+//Дана последовательность целых чисел.
+// Вывести отсортированный список в порядке возрастания.
+public class FirstLab {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (String x : args)
+        {
+            list.add(Integer.parseInt(x));
+        }
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
