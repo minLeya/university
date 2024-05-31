f55ae2645f6c649ada530434e409520c37777ab7
Leya <108834527+minLeya@users.noreply.github.com>
2024-05-31 19:24:57 +0300

    upd 7th lab web

--- 6 term/web/7-th lab/task/server.js
+++ 6 term/web/7-th lab/task/server.js
@@ -7,7 +7,7 @@ var express = require("express"),
             "master": "Иванов Иван Иванович",
             "title": "Мужская стрижка",
             "price": "500",
-            "datetime": ["2024-06-01 10:00", "2024-06-02 14:00"]
+            "datetime": ["2024-06-01 10:30", "2024-06-02 14:00"]
         },
         {
             "category": "Стрижка",
@@ -26,18 +26,17 @@ var express = require("express"),
     ];
 
 app.use(express.static(__dirname + "/client"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 http.createServer(app).listen(3000);
 
 app.get("/services.json", function (req, res) {
     res.json(services);
 });
 
+app.use(express.urlencoded());
 app.post("/services", function (req, res) {
     console.log("Данные были отправлены на сервер!");
     var newService = req.body;
+    console.log(newService);
     services.push(newService);
     res.json({ "message": "Вы размещаетесь на сервере!" });
 });
